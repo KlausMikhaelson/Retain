5e0109661cde7a378edb48ccee049d8160370ecd
KlausMikhaelson <satyamsingh5076@gmail.com>
2024-05-11 17:20:31 -0600

    added sorting of links ad delete option

--- src/popup/Popup.tsx
+++ src/popup/Popup.tsx
@@ -19,7 +19,7 @@ export const Popup = () => {
   useEffect(() => {
     console.log("currentWebsiteLink", currentWebsiteLink);
   }, [currentWebsiteLink])
-  
+
   useEffect(() => {
     const fetchData = async () => {
       try {
@@ -55,6 +55,13 @@ export const Popup = () => {
     fetchData();
   }, [currentWebsiteLink])
 
+  const deleteKeyValuePair = (key: string) => {
+    chrome.storage.local.remove(key, () => {
+      const newKeyValuePairs = keyValuePairs.filter((keyValuePair) => keyValuePair.key !== key);
+      setKeyValuePairs(newKeyValuePairs);
+    });
+  }
+
   useEffect(() => {
     console.log("keyValuePairs", keyValuePairs);
   }, [keyValuePairs]);
@@ -63,15 +70,27 @@ export const Popup = () => {
     <main>
       <h3>Input Stash</h3>
       <div className="key-value-pair-list">
-        <div className="key-value-pair">
+        <div className="key-value-pair" style={{margin: "20px"}}>
           <div>Input Stash for the current tab</div>
         </div>
-        {keyValuePairs.map((keyValuePair, index) => (
-          <div style={{display: "flex", alignItems: "center"}} className="key-value-pair" key={index}>
-            <a style={{textDecoration:"none", padding: "5px 10px", margin: "10px", borderRadius: "5px", backgroundColor: "#1877F2", color: "black"}} className="key" href={keyValuePair.key.split(/-\d+$/)[0]} target="_blank">Link</a>
-            <div style={{alignItems: "start", textAlign: "start"}} className="value">{keyValuePair.value}</div>
-          </div>
-        ))}
+        {keyValuePairs.map((keyValuePair, index) => {
+          const sortedWithTimestamp = keyValuePairs.sort((a, b) => {
+            const aTimestamp = parseInt(a.key.split("-")[1]);
+            const bTimestamp = parseInt(b.key.split("-")[1]);
+            return bTimestamp - aTimestamp;
+          }
+          );
+
+          return (
+            <div style={{ display: "flex", alignItems: "center", justifyContent: "space-between", padding: "8px" }} className="key-value-pair" key={index}>
+              <div style={{display:"flex"}}>
+              <a style={{ textDecoration: "none",height: "15px", padding: "5px 10px", marginRight: "10px", borderRadius: "5px", backgroundColor: "#1877F2", color: "black" }} className="key" href={keyValuePair.key.split(/-\d+$/)[0]} target="_blank">Link</a>
+              <div style={{ alignItems: "start", textAlign: "start" }} className="value">{keyValuePair.value}</div>
+              </div>
+              <button style={{ marginLeft: "10px" }} onClick={() => deleteKeyValuePair(keyValuePair.key)}>Delete</button>
+            </div>
+          )
+        })}
       </div>
     </main>
   )
